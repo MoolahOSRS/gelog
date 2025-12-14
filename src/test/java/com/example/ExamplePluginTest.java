9effd56e797165bf7238757644cf43214005a372
Moolah <67553387+MoolahOSRS@users.noreply.github.com>
2025-12-14 08:53:45 -0700

    Update ExamplePluginTest.java

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package MoolahOSRS.geLog;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class geLogPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(geLogPlugin.class);
 		RuneLite.main(args);
 	}
 }
